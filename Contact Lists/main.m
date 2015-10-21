ad5577edb2046cbe308fa3be595e7f0942eac18f
Alex Robertson <hello_im_alex@hotmail.com>
2015-10-20 17:07:55 -0700

    initial commit with Input Collector

---
+++ Contact Lists/main.m
@@ -0,0 +1,28 @@
+//
+//  main.m
+//  Contact Lists
+//
+//  Created by Alex on 2015-10-20.
+//  Copyright © 2015 Alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "InputCollector.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        
+        InputCollector *inputCollector = [[InputCollector alloc] init];
+    
+        NSString* input = [inputCollector inputForPrompt:@"\nWhat would you like to do next?\n\n new - Create a new contact\n list - List all contacts\n quit - Exit Application"];
+        
+        if ([input isEqualToString:@"quit"]) {
+            
+            NSLog(@"Fine then. Goodbye.");
+            
+        }
+        
+    }
+    return 0;
+}
